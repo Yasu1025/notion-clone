6ebb0626031fe6673f4f1d8bcef8fead7d095ed9
Yasu1025 <sillyjp25@gmail.com>
2024-12-21 20:08:14 +0900

    [SERVER] cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express()
 const PORT = parseInt(process.env.PORT || '3000', 10) // Default 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:5173' }))
 
 setupRoutes(app)
 
